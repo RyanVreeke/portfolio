93c7cce135982257a7d56731a04f31f048b36430
Ryan Vreeke <rvreeke0229@gmail.com>
2025-03-17 12:40:56 -0700

    feat: Add UI updates for quality. (#3)

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,14 +12,16 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
-      {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
-      })}
-      <Link href="/search">
-        <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-primary" />
-      </Link>
-    </nav>
+    <div className="bg-gradient-to-r from-slate-800 to-slate-900 px-3 py-2 rounded-lg shadow-lg">
+      <nav className="flex gap-3 items-center">
+        {navItems.map(({ link }, i) => {
+          return <CMSLink key={i} {...link} appearance="link" />
+        })}
+        <Link href="/search">
+          <span className="sr-only">Search</span>
+          <SearchIcon className="w-5 text-primary" />
+        </Link>
+      </nav>
+    </div>
   )
 }
